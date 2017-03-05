2709019cae44e7b754ba5831d4229029c34efca8
Alvin Yuen <alvinyuen628@gmail.com>
2017-03-04 22:03:47 -0600

    html webpack plugin setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: 'dist',
+    filename: 'app.bundle.js'
+  },
+  plugins:
+    [
+      new HtmlWebpackPlugin({
+        title: 'Custom template',
+        minify: {
+          collapseWhitespace: true
+        },
+        hash: true,
+        template: './src/index.html',
+      })
+  ]
+}
